973a5b4ec071c8b29b911ed8637bf3fa08b451f7
Martin-Louis Bright <mlbright@gmail.com>
2019-02-23 00:02:13 -0500

    version two

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,19 @@ extern crate regex;
 use regex::Regex;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufReader;
+use std::io::{self, BufReader};
 
-fn main() {
-    let date_re = Regex::new(r"[\d+/\w+/\d{4}:\d\d:\d\d:\d\d\.\d+]").unwrap();
+fn main() -> io::Result<()> {
+    let date_re = Regex::new(r"[\d+/\w+/\d\d\d\d:\d\d:\d\d:\d\d\.\d+]").unwrap();
+    let f = File::open("haproxy.log")?;
+    let f = BufReader::new(f);
 
-    let log_path = "haproxy.log";
-    let f = File::open(log_path).expect("Could not open file!");
-    let mut buffered = BufReader::new(f);
-
-    let mut buf = String::new();
-    loop {
-        match buffered.read_line(&mut buf) {
-            Ok(n) => {
-                if n > 0 {
-                    {
-                        let line = buf.trim_right();
-                        if date_re.is_match(line) {
-                            println!("{}", line);
-                        }
-                    }
-                    buf.clear();
-                }
-            }
-            Err(_) => break,
+    for line in f.lines() {
+        let s = line.unwrap();
+        if date_re.is_match(s.trim_right()) {
+            println!("{}", s);
         }
     }
+
+    Ok(())
 }
