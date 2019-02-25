302267e461182e6b79dc830056af0db6f859038c
Martin-Louis Bright <mlbright@gmail.com>
2019-02-24 22:37:33 -0500

    better

--- logslice.pl
+++ logslice.pl
@@ -4,16 +4,28 @@ use strict;
 use warnings;
 use feature qw{ say };
 use Getopt::Long;
+use Time::Piece;
 
 my $opts = {};
 GetOptions( $opts, "--begin=s", "--end=s", "--regex=s", "--format=s",
   "--chunk=i" );
 
 my $regex = qr/$opts->{regex}/;
+my $chunk = $opts->{chunk} || 1000;
 
-while ( my $line = <> ) {
-  chomp $line;
-  if ( $line =~ $regex ) {
-    say $line;
+my @buffer;
+while ( <> ) {
+  push @buffer, $_;
+  if (@buffer == $chunk || eof) {
+    for my $line (@buffer) {
+      chomp $line;
+      my ($time) = $line =~ $regex;
+        Time::Piece->strptime($time, 
+        say $line;
+      }
+    }
+
+    my( $low, $high ) = binsearch_range { $a <=> $b }, $opt->{begin}, $opt->{end}, @haystack;
+    @buffer = ();
   }
 }
