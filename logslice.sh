3f182b12c62aa38af15347c5861ea36b4bb4319a
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2019-02-25 21:11:49 -0500

    test 2 different timestamps

--- logslice.sh
+++ logslice.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 set -e
 
-# regex='\[(\d+\/\w+\/\d\d\d\d:\d\d:\d\d:\d\d\.\d+)\]'
-regex='^([A-Za-z]+ \d+ \d\d:\d\d:\d\d) '
-# format='%d/%B/%Y:%T.%3N'
-format='%b %d %T'
+time wc -l haproxy.log
 
-time wc -l sample.log
-
-time perl logslice-binary-search.pl \
+time perl logslice.pl \
   --year=2019 \
-  --regex="$regex" \
-  --begin="2019-02-20T06:00:00" \
-  --end="2019-02-23T07:00:00" \
-  --format="$format" < sample.log | wc -l
+  --regex='^([A-Za-z]+ \d+ \d\d:\d\d:\d\d) ' \
+  --begin="2019-02-22T06:00:00" \
+  --end="2019-02-22T07:00:00" \
+  --format='%b %d %T' < haproxy.log | wc -l
 
-time perl logslice-naive.pl \
+time perl logslice.pl \
   --year=2019 \
-  --regex="$regex" \
-  --begin="2019-02-20T06:00:00" \
-  --end="2019-02-23T07:00:00" \
-  --format="$format" < sample.log | wc -l
+  --regex='\[(\d+\/\w+\/\d\d\d\d:\d\d:\d\d:\d\d\.\d+)\]' \
+  --begin="2019-02-22T06:00:00" \
+  --end="2019-02-22T07:00:00" \
+  --format='%d/%B/%Y:%T.%3N' < haproxy.log | wc -l
