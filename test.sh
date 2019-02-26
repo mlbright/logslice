7f457c8769b279b5863899408abfb9bfe5f39678
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2019-02-25 21:18:14 -0500

    more logs

--- test.sh
+++ test.sh
@@ -16,3 +16,10 @@ time perl logslice.pl \
   --begin="2019-02-22T06:00:00" \
   --end="2019-02-22T07:00:00" \
   --format='%d/%B/%Y:%T.%3N' < haproxy.log | wc -l
+
+time perl logslice.pl \
+  --year=2019 \
+  --regex='^([A-Za-z]+ \d+ \d\d:\d\d:\d\d) ' \
+  --begin="2019-02-22T06:00:00" \
+  --end="2019-02-22T07:00:00" \
+  --format='%b %d %T' < github-audit.log | wc -l
