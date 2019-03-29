99cada163ec50a66bf9cf5ff3d271b41ed0dd5cd
Martin-Louis Bright <mlbright@gmail.com>
2019-03-29 10:45:15 -0400

    output test titles

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Rust version:
+echo "Rust version:"
 time cargo run --release -- --start="2019-03-29T10:07:00" --finish="2019-03-29T10:08:00" --regexp="now=\"([^\"]+)\"" --time-format="%Y-%m-%dT%H:%M:%S%:z" < unicorn.log > rust.slice 2> rust.err
 
-# Perl version:
+echo "Perl version:"
 time perl scripts/timegrep.pl --start="2019-03-29T10:07:00" --finish="2019-03-29T10:08:00" --regexp="now=\"([^\"]+)+00:00\"" --time-format="%Y-%m-%dT%H:%M:%S" < unicorn.log> perl.slice 2> perl.err 
