daf48212df25f738b01d16c327efdcaf623a033c
Martin-Louis Bright <mlbright@gmail.com>
2019-03-29 10:40:49 -0400

    benchmark

---
+++ scripts/bench.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Rust version:
+time cargo run --release -- --start="2019-03-29T10:07:00" --finish="2019-03-29T10:08:00" --regexp="now=\"([^\"]+)\"" --time-format="%Y-%m-%dT%H:%M:%S%:z" < unicorn.log > rust.slice 2> rust.err
+
+# Perl version:
+time perl scripts/timegrep.pl --start="2019-03-29T10:07:00" --finish="2019-03-29T10:08:00" --regexp="now=\"([^\"]+)+00:00\"" --time-format="%Y-%m-%dT%H:%M:%S" < unicorn.log> perl.slice 2> perl.err 
--- test.sh
+++ test.sh
@@ -3,23 +3,23 @@ set -e
 
 time wc -l haproxy.log
 
-time perl logslice.pl \
+time perl timegrep.pl \
   --year=2019 \
-  --regex='^([A-Za-z]+ \d+ \d\d:\d\d:\d\d) ' \
+  --regexp='^([A-Za-z]+ \d+ \d\d:\d\d:\d\d) ' \
   --begin="2019-02-22T06:00:00" \
   --end="2019-02-22T07:00:00" \
-  --format='%b %d %T' < haproxy.log | wc -l
+  --time-format='%b %d %T' < haproxy.log | wc -l
 
-time perl logslice.pl \
+time perl timegrep.pl \
   --year=2019 \
-  --regex='\[(\d+\/\w+\/\d\d\d\d:\d\d:\d\d:\d\d\.\d+)\]' \
+  --regexp='\[(\d+\/\w+\/\d\d\d\d:\d\d:\d\d:\d\d\.\d+)\]' \
   --begin="2019-02-22T06:00:00" \
   --end="2019-02-22T07:00:00" \
-  --format='%d/%B/%Y:%T.%3N' < haproxy.log | wc -l
+  --time-format='%d/%B/%Y:%T.%3N' < haproxy.log | wc -l
 
-time perl logslice.pl \
+time perl timegrep.pl \
   --year=2019 \
-  --regex='^([A-Za-z]+ \d+ \d\d:\d\d:\d\d) ' \
+  --regexp='^([A-Za-z]+ \d+ \d\d:\d\d:\d\d) ' \
   --begin="2019-02-22T06:00:00" \
   --end="2019-02-22T07:00:00" \
-  --format='%b %d %T' < github-audit.log | wc -l
+  --time-format='%b %d %T' < github-audit.log | wc -l
